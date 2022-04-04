71da4f7332cf74848f8182bc28ff3a236f3742c1
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2022-04-04 10:03:34 -0500

    search bar maybe

--- .github/OldGloryLPB/Pages/Customers/Index.cshtml.cs
+++ .github/OldGloryLPB/Pages/Customers/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OldGloryLPB.Data;
 using OldGloryLPB.Models;
@@ -21,9 +22,22 @@ namespace OldGloryLPB.Pages.Customers
         }
 
         public IList<Customer> Customer { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Customers { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CustomerName { get; set; }
+
 
         public async Task OnGetAsync()
         {
+            var customers = from c in _context.Customer
+                            select c;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                customers = customers.Where(s => s.Title.Contains(SearchString));
+            }
+
             Customer = await _context.Customer.ToListAsync();
         }
     }
